470a4dfe91f12174d1730af0899a072a51f06d2f
Ruarai Tobin <ruarai.tobin@unimelb.edu.au>
2021-03-24 10:31:38 +1100

    save model stats during prediction

--- R/produce_predictions.R
+++ R/produce_predictions.R
@@ -43,38 +43,67 @@ print("Starting prediction loop...")
 print(paste0("With ", n_core, " cores."))
 
 
-model_preds_seasia <- foreach(i=1:length(model_list), .packages = c('dismo',
-                                                                    'snowfall',
-                                                                    'seegSDM')) %dopar% {
-  print(paste0("Predicting human-SEAsia for model ", i, " of ", length(model_list)))
-  
-  a <- Sys.time()
-  
-  m <- model_list[[i]]
-  
-  pred <- predict(prediction_covs, m$model, type="response", n.trees = m$model$n.trees)
-  
-  print(paste0("Predicted for model ", i, " in ", Sys.time() - a, "."))
-  
-  return(pred)
-}
+model_preds_seasia <- foreach(i=1:length(model_list), 
+                              .packages = c('dismo',
+                                            'snowfall',
+                                            'seegSDM')) %dopar%
+  {
+    print(paste0("Predicting human-SEAsia for model ", i, " of ", length(model_list)))
+    
+    a <- Sys.time()
+    
+    m <- model_list[[i]]
+    
+    pred <- predict(prediction_covs, m$model, type="response", n.trees = m$model$n.trees)
+    
+    print(paste0("Predicted for model ", i, " in ", Sys.time() - a, "."))
+    
+    return(pred)
+    
+  }
+
 
 model_preds_seasia <- getValues(stack(model_preds_seasia))
 
+# get cv statistics
+stat_lis <- lapply(model_list, getStats)
+
+# convert the stats list into a matrix using the do.call function
+stats <- do.call("rbind", stat_lis)
+
+# save the relative influence scores
+relinf_list <- lapply(model_list, function(x) {
+  as.data.frame(t(x$model$contributions[,2, drop=FALSE]))
+})
+
+relinf <- bind_rows(relinf_list)
+
+
+
+
 print("Saving predictions.")
 
-out_dir <- paste0("output/update/predictions/", run_unique_name, "/")
-if(!dir.exists(out_dir)){
-  dir.create(out_dir, recursive = TRUE)
+
+
+
+out_dir_stats <- paste0("output/update/model_stats/", run_unique_name, "/")
+if(!dir.exists(out_dir_stats)){
+  dir.create(out_dir_stats, recursive = TRUE)
+}
+write.csv(stats,paste0(out_dir_stats,'stats.csv'))
+
+out_dir_rel_inf <- paste0("output/update/model_rel_inf/", run_unique_name, "/")
+if(!dir.exists(out_dir_stats)){
+  dir.create(out_dir_stats, recursive = TRUE)
+}
+write.csv(relinf, file = paste0(out_dir_rel_inf, 'relative_influence.csv'))
+
+
+out_dir_predictions <- paste0("output/update/predictions/", run_unique_name, "/")
+if(!dir.exists(out_dir_predictions)){
+  dir.create(out_dir_predictions, recursive = TRUE)
 }
 
 saveRDS(model_preds_seasia,
-        file = paste0(out_dir, this_task_id, "_model_pred.Rds"),
+        file = paste0(out_dir_predictions, this_task_id, "_model_pred.Rds"),
         compress=FALSE)
-
-
-
-
-
-
-
